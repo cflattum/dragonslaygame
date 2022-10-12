7b90ec8c1c51e5a2415baf2411bb33d1e32e48c3
Christopher Flattum <cflattum@usc.edu>
2022-10-12 15:59:25 -0700

    added player struct, mapping, private variables

--- contracts/slaythedragon.sol
+++ contracts/slaythedragon.sol
@@ -3,14 +3,45 @@ pragma solidity ^0.8.9;
 
 import "../node_modules/@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import "../node_modules/@openzeppelin/contracts/access/Ownable.sol"; 
+import "../node_modules/@openzeppelin/contracts/security/Pausable.sol"; 
 
-contract slaythedragon is ERC1155, Ownable 
+//things need to check for
+
+contract slaythedragon is ERC1155, Ownable, Pausable
 {
+    //using Strings for string;
+
     address private thisContract;
+    address private mainContract;
+    mapping (address -> player) private _players;
 
+    struct player{
+        address player;
+        bool awarded;
+        uint timeLastPlayed;
+    }
 
-     constructor() public ERC1155("URI PATH TO BE EDITED"){
+     constructor() ERC1155("URI PATH TO BE EDITED"){
         thisContract = address(this);
+     }
+
+     function mintReward(address toMint) external
+     {
+        require(condition);
+     }
+
+     function changeMainContractAddy(address newAddy) external onlyOwner
+     {
+        mainContract = newAddy;
+     }
+
+     function checkReward(address player) returns (bool allowed)
+     {
+        require(_drivers[msg.sender].awarded == false), "Already minted reward";
+     }
+     function checkLastPlay(address player) returns (bool allowed)
+     {
+
      }
      
 
