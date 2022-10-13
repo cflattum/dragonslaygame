b1ca9386ad847b0b36ca755af15d8cef72e8aa59
Christopher Flattum <cflattum@usc.edu>
2022-10-12 17:19:06 -0700

    continue writing neccesary functions

--- contracts/slaythedragon.sol
+++ contracts/slaythedragon.sol
@@ -6,28 +6,44 @@ import "../node_modules/@openzeppelin/contracts/access/Ownable.sol";
 import "../node_modules/@openzeppelin/contracts/security/Pausable.sol"; 
 
 //things need to check for
-
+/**
+* @title SlayTheDragon: a erc 1155 minting contract for rewarding minigames in the Knights who say Nah
+* @author Christopher Carl Flattum
+* @notice Mapping of player structs holds crucial information
+* @notice deployer must set main contract address with functions in file
+* @notice can change URI via default 1155 function for reuse
+*/
 contract slaythedragon is ERC1155, Ownable, Pausable
 {
     //using Strings for string;
 
     address private thisContract;
     address private mainContract;
-    mapping (address -> player) private _players;
+
+    mapping (address => player) private _players;
 
     struct player{
-        address player;
+        //address player;
         bool awarded;
-        uint timeLastPlayed;
+        uint256 timeLastPlayed;
+        bool approved;
     }
 
      constructor() ERC1155("URI PATH TO BE EDITED"){
         thisContract = address(this);
      }
 
-     function mintReward(address toMint) external
+     function mintReward(address toMint) external onlyOwner
      {
-        require(condition);
+        //requires that 1. Have not minted before
+        require(_players[toMint].awarded == false, "Already minted reward");
+        //2. have won game / are approved to mint
+        require( _players[toMint].approved == true, "Must win game to be able to mint");
+
+        //change player variables to signify awarded state
+        _players[toMint].awarded = true;
+        //change player variable to remove their approval 
+        _players[toMint].approved = false;
      }
 
      function changeMainContractAddy(address newAddy) external onlyOwner
@@ -35,15 +51,49 @@ contract slaythedragon is ERC1155, Ownable, Pausable
         mainContract = newAddy;
      }
 
-     function checkReward(address player) returns (bool allowed)
+     function checkReward(address play) public view returns (bool allowed)
      {
-        require(_drivers[msg.sender].awarded == false), "Already minted reward";
+        if(_players[play].awarded == false)
+        {
+        return true;
+        }
+        else
+        {
+        return false;
+        }
      }
-     function checkLastPlay(address player) returns (bool allowed)
+     //returns true if have not played in two days
+     function checkLastPlay(address play) public view returns (bool allowed)
      {
+        //if time last played + 2 days is before now, 
+        if((_players[play].timeLastPlayed + 86400) < block.timestamp)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+     }
 
+     function addWhitelist(address play) public onlyOwner 
+     {
+        //set whitelist variable to true for a certain address play
+        //called by the website when the player wins the game
+        _players[play].approved = true;
      }
      
+     function receivePlay(address play, bool result) external onlyOwner
+     {
+        //when player plays, the website calls this function to add results
+        _players[play].timeLastPlayed = block.timestamp;
+
+        //if we won, add to whitelist
+        if(result == true)
+        {
+            addWhitelist(play);
+        }
+     }
 
 
 }
