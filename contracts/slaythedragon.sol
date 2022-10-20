e573875cea46f231602aa1cc682b851c907ace13
Christopher Flattum <cflattum@usc.edu>
2022-10-20 15:01:05 -0700

    ECDSA changes; most function rewritten

--- contracts/slaythedragon.sol
+++ contracts/slaythedragon.sol
@@ -3,10 +3,14 @@ pragma solidity ^0.8.9;
 
 import "../node_modules/@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import "../node_modules/@openzeppelin/contracts/access/Ownable.sol"; 
-import "../node_modules/@openzeppelin/contracts/security/Pausable.sol"; 
+import "../node_modules/@openzeppelin/contracts/security/Pausable.sol";
+import "../node_modules/@openzeppelin/contracts/utils/Counters.sol";
+import "../node_modules/@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 
 //things need to check for 
 //1. Function Visibility
+
+//very modular contract: could be made into a couple different functions instead 
 
 /**
 * @title SlayTheDragon: a erc 1155 minting contract for rewarding minigames in the Knights who say Nah
@@ -17,50 +21,67 @@ import "../node_modules/@openzeppelin/contracts/security/Pausable.sol";
 */
 contract slaythedragon is ERC1155, Ownable, Pausable
 {
-    //using Strings for string;
+   //using Strings for string;
+   using ECDSA for bytes32;
 
-    address private thisContract;
-    address private mainContract;
+   address private thisContract;
+   address private mainContract;
 
-    mapping (address => player) private _players;
+   address private _signerAddress;
 
-    struct player{
+   uint32 private _nftID = 1;
+
+   mapping (address => player) private _players;
+
+   //player struct, incluiding mapping from NFT to bool to check if already awarded
+   struct player{
         //address player;
-        bool awarded;
+        mapping(uint => bool) awarded;
         uint256 timeLastPlayed;
-        bool approved;
     }
 
      constructor() ERC1155("URI PATH TO BE EDITED"){
         thisContract = address(this);
      }
 
-     function mintReward(address toMint) external onlyOwner
+     function mintReward(address toMint, bytes calldata sig) public
      {
         //requires that 1. Have not minted before
-        require(_players[toMint].awarded == false, "Already minted reward");
-        //2. have won game / are approved to mint
-        require( _players[toMint].approved == true, "Must win game to be able to mint");
+        require(_players[toMint].awarded[_nftID] == false, "Already minted reward");
+        //2. checks that the signature passed was signed by a wallet of our choosing
+        require(_signerAddress == keccak256(
+               abi.encodePacked("\x19Ethereum Signed Message:\n32",
+               bytes32(uint256(uint160(msg.sender)))
+            )
+        ).recover(sig), "no but nice try though!");
 
         //change player variables to signify awarded state
-        _players[toMint].awarded = true;
-        //change player variable to remove their approval 
-        //this is done as for future integration sake; could be better as an array of bools signifying the first, second, etc
-        _players[toMint].approved = false;
+        _players[toMint].awarded[_nftID] = true;
 
-        //UNFINISHED.. what ID?
-        _mint(toMint, id, 1, 0);
+        //mints to address, using ID, one NFT, no extra data
+        _mint(toMint, _nftID, 1, "");
      }
 
+     function setSigner(address signerAddress) public onlyOwner
+     {
+         _signerAddress = signerAddress;
+     }
+
+     function changeReward(uint id) public onlyOwner
+     {
+         _nftID = id;
+     }
+
+    //not actually needed with current design; iteration upon design may require
      function changeMainContractAddy(address newAddy) external onlyOwner
      {
         mainContract = newAddy;
      }
 
     //returns true if has been awarded already, false if not 
-     function beenRewarded(address play) public view returns (bool allowed)
+     function beenRewarded(address play, uint id) public view returns (bool allowed)
      {
-        if(_players[play].awarded == false)
+        if(_players[play].awarded[id] == false)
         {
         return false;
         }
@@ -70,37 +91,30 @@ contract slaythedragon is ERC1155, Ownable, Pausable
         }
      }
 
-     //returns true if have not played in two days
-     function checkLastPlay(address play) public view returns (bool allowed)
+     //This transaction is signed by a player as a prerequisite to playing;
+     //notates the time they tried to play to prevent them from playing again too soon in the future
+     function attemptPlay(address play) public view returns (bool allowed)
      {
-        //if time last played + 2 days is before now,
-        //2*24*60*60= 2 days in seconds / 172800 seconds
-        if((_players[play].timeLastPlayed + 172800) < block.timestamp)
-        {
-            return true;
-        }
-        else
-        {
-            return false;
-        }
+        _players[play].timeLastPlayed = block.timestamp;
      }
 
-       //this function checks if the wallet is ok to try playing - 
+       //this function checks if the wallet is ok to try playing - this is checked before they are allowed to try
+       //called before the above function attemptPlay which notates into memory they are trying to play
      function checkBeforePlaying(address play) public view returns (bool oktoPlay)
      {
-        if(checkLastPlay(play) == true && beenRewarded(play) == false)
+        if(_players[play].awarded[_nftID] == false && ((_players[play].timeLastPlayed + 172800) < block.timestamp))
         {
             return true;
         }
      }
 
-    //internal (?) function that puts on a 'whitelist' to allow minting
-     function addWhitelist(address play) public onlyOwner 
-     {
-        //set whitelist variable to true for a certain address play
-        //called by the website when the player wins the game
-        _players[play].approved = true;
-     }
+   //  //internal (?) function that puts on a 'whitelist' to allow minting
+   //   function addWhitelist(address play) public onlyOwner 
+   //   {
+   //      //set whitelist variable to true for a certain address play
+   //      //called by the website when the player wins the game
+   //      _players[play].approved = true;
+   //   }
      
      //function that is called during/after playing - signaling the win/loss, and adding timestamps
      function receivePlay(address play, bool result) external onlyOwner
@@ -111,7 +125,16 @@ contract slaythedragon is ERC1155, Ownable, Pausable
         //if we won, add to whitelist
         if(result == true)
         {
-            addWhitelist(play);
+            //addWhitelist(play);
+
+            //design question: Should immeaditely mint from here?
+            //its possible! and maybe the easiest way - need to figure out how to give permissions 
+            
+            //maybe not; need to record that they played, and the result.
+            //well - if they lose, this if isnt run, so...
+
+            //NOT FINAL
+            mintReward(play);
         }
      }
 
